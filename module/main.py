055f4a939603d4455a377f1086419d70f407d224
passionkmk <passion.kmk@gmail.com>
2021-02-11 21:43:48 +0900

    feat(common): Add module

---
+++ module/main.py
@@ -0,0 +1,9 @@
+from module.mymath import add
+import sys
+import mymath
+
+# 왜 안되는 잘모르겠음.
+
+if __name__ == "__main__":
+    print(add(sys.argv[0], sys.argv[1]))
+    print(sub(sys.argv[0], sys.argv[1]))
